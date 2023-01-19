b2bc9278dae9b2badf0308afe6fb7bc3d8da0a1f
홍성민 <seanhong2000@gmail.com>
2023-01-19 17:47:01 +0900

    [Day16] UIKit

---
+++ Day16/Day16/Day16/ViewController.swift
@@ -0,0 +1,40 @@
+//
+//  ViewController.swift
+//  Day16
+//
+//  Created by Sean Hong on 2023/01/19.
+//
+
+import UIKit
+
+class ViewController: UITableViewController {
+    var pictures = [String]()
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        let fileManager = FileManager.default
+        let path = Bundle.main.resourcePath!
+        let items = try! fileManager.contentsOfDirectory(atPath: path)
+        
+        for item in items {
+            if item.hasPrefix("nssl") {
+                pictures.append(item)
+                print(pictures)
+            }
+        }
+    }
+
+    override func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return pictures.count
+    }
+    
+    override func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        let cell = tableView.dequeueReusableCell(withIdentifier: "Picture", for: indexPath)
+        cell.textLabel?.text = pictures[indexPath.row]
+        return cell
+    }
+    
+    
+
+}
+
