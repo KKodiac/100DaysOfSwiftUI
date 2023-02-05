ca7339001b8b7490121fb996ea27ad16df597b79
홍성민 <seanhong2000@gmail.com>
2023-02-06 01:14:27 +0900

    [Day33]

---
+++ Day32/Day32/Day32/Day32App.swift
@@ -0,0 +1,17 @@
+//
+//  Day32App.swift
+//  Day32
+//
+//  Created by Sean Hong on 2023/02/05.
+//
+
+import SwiftUI
+
+@main
+struct Day32App: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
